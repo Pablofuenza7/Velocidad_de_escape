be4ad22a82f6d109cfb58ea1396ee61ef39b1f43
Pablofuenza7 <pafu4012@gmail.com>
2020-08-23 20:21:03 -0400

    first commit

---
+++ escape.rb
@@ -0,0 +1,9 @@
+
+g = ARGV[0].to_i #Argumento Gravedad
+
+r = ARGV[1].to_i #Argumento Radio
+
+total = Math.sqrt(2*g*r) #calcúlo aritmético
+
+puts "La Velocidad de escape es #{total}" #Mensaje con resultado
+
